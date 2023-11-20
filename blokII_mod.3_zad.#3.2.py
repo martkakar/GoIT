334d8a13bab103d33a8a73861fb901cbf00c5a21
martkakar <136683683+martkakar@users.noreply.github.com>
2023-11-20 17:23:11 +0100

    blokII_mod.3_zad.#3.2.py

---
+++ blokII_mod.3_zad.#3.2.py
@@ -0,0 +1,26 @@
+import logging
+from multiprocessing import Pool, cpu_count
+
+logging.basicConfig(level=logging.INFO)
+
+def factorize_single(number):
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+def factorize(*numbers):
+    logging.info("Starting calculations...")
+    with Pool(cpu_count()) as pool:
+        result = pool.map(factorize_single, numbers)
+    logging.info("Calculations completed.")
+    return result if len(numbers) > 1 else result[0]
+
+if __name__ == '__main__':
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
